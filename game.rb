657eda08e1b1839a4cf8608a2561de3aa6b5ef5f
Rodolfo <rudolfrce@gmail.com>
2022-03-13 19:38:41 -0700

    Refactor winner_detector to @winner_detector

--- game.rb
+++ game.rb
@@ -67,14 +67,13 @@ module Game
     end
 
     def search_winner(board)
-      winner_detector = false
+      @winner_detector = false
 
       if board[0][0] != " "
         if ((board[0][0] == board[1][1] && board[0][0] == board[2][2]) ||
             (board[0][0] == board[0][1] && board[0][0] == board[0][2]) ||
             (board[0][0] == board[1][0] && board[0][0] == board[2][0]))
           winner board[0][0]
-          winner_detector = true
         end
       end
 
@@ -82,36 +81,33 @@ module Game
         if ((board[0][2] == board[1][1] && board[0][2] == board[2][0]) ||
             (board[0][2] == board[1][2] && board[0][2] == board[2][2]))
           winner board[0][2]
-          winner_detector = true
         end
       end
 
       if board[0][1] != " "
         if ((board[0][1] == board[1][1] && board[0][1] == board[2][1]))
           winner board[0][1]
-          winner_detector = true
         end
       end
 
       if board[1][0] != " "
         if (board[1][0] == board[1][1] && board[1][0] == board[1][2])
           winner board[1][0]
-          winner_detector = true
         end
       end
 
       if board[2][0] != " "
         if (board[2][0] == board[2][1] && board[2][0] == board[2][2])
           winner board[2][0]
-          winner_detector = true
         end
       end
 
-      winner_detector
+      @winner_detector
     end
 
     def winner(board_val)
       puts "WINS #{board_val}!"
+      @winner_detector = true
     end
   end
 end
